b232f1c8e1aca12c7a361a90c51e3243b244cc85
paddy3 <paddytree3@gmail.com>
2015-08-23 17:50:06 +0200

    can catch all signals

--- src/signal_catch.cpp
+++ src/signal_catch.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
-using namespace std;
+void my_handler(int s){
+           printf("Caught signal %d\n",s);
+}
 
-int main() {
+int main(int argc,char** argv)
+{
 
-	cout << "hi" << endl;
+   struct sigaction sigIntHandler;
 
+   sigIntHandler.sa_handler = my_handler;
+   sigemptyset(&sigIntHandler.sa_mask);
+   sigIntHandler.sa_flags = 0;
+
+   for (int i=1;i<32;++i)
+	   sigaction(i, &sigIntHandler, NULL);
+
+   while(1);
+
+   return 0;
 }
