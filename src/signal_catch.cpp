c2621f9d9469fd6dc61dece5ad2b236867207184
paddy3 <paddytree3@gmail.com>
2015-08-23 18:02:46 +0200

    writes status in signal_cought.txt

--- src/signal_catch.cpp
+++ src/signal_catch.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <ctime>
 
+std::ofstream OUTFILE("signal_cought.txt");
+
 void my_handler(int s){
     time_t t = time(0);   // get time now
     struct tm * now = localtime( & t );
 
-           std::cout << "Caught signal " << s;
-           std::cout << " at ";
-           std::cout << (now->tm_year + 1900) << '-'
-        	         << (now->tm_mon + 1) << '-'
-        	         <<  now->tm_mday << ' '
-					 <<  now->tm_hour << ':'
-					 <<  now->tm_min << ':'
-					 <<  now->tm_sec
-        	         << std::endl;
+           OUTFILE << "Caught signal " << s
+        		   << " at "
+				   << (now->tm_year + 1900) << '-'
+        	       << (now->tm_mon + 1) << '-'
+        	       <<  now->tm_mday << ' '
+				   <<  now->tm_hour << ':'
+				   <<  now->tm_min << ':'
+				   <<  now->tm_sec
+				   << std::endl;
 }
 
 int main(int argc,char** argv)
